07ffeeb45c8db7b6cd29e59b7f45b52b5f72d339
Avyaan Verma <avyaanver@gmail.com>
2023-06-09 11:50:53 +0530

    s cmmit

---
+++ args.py
@@ -0,0 +1,11 @@
+# *args = parameter tat will pack all arguments into a tuple
+# useful so that a function that can accept a varying amount of arguments 
+
+def add(*stuff):
+    sum=0
+    stuff=list(stuff)
+    for i in stuff:
+        sum+=i
+    return sum 
+# print(add(1,2,3)) This becomes unuseful
+
