c4470811f7acaef67bfd817e9091cb3500d6e5fc
Avyaanver <Avyaanver@gmail.com>
2023-06-12 21:20:31 +0530

    defining a function

---
+++ Password Manager/brocode/practice/function.py
@@ -0,0 +1,24 @@
+#function = A block of reusable code
+
+# def happy_birthday(age, name): # ORDER Matters in this type
+#     print(f"Happy birthday to {name} !")
+#     print(f"You  are {age} old!")
+#     print("Happy birthday to you !")
+#     print()
+#
+# happy_birthday("Avyaan", 18)
+# happy_birthday("Bro", 15)
+
+# ANY data you send via a function is an argument
+
+
+
+# def display_invoice(username,amount, due_date):
+#     print(f"Hello {username}")
+#     print(f"Your bill of ${amount:.2f} is due : {due_date}")
+#
+# display_invoice("Avyaan", 42.50, "04/03")
+
+
+
+# return = statement used to end a function and send a result back to the caller
