822dd3e954613ac1e78419b264bd420813ace97b
Avyaan Verma <114806843+avyaanverma@users.noreply.github.com>
2023-06-20 20:19:46 +0530

    ques list comprehension complete

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -2,7 +2,7 @@ if __name__ == '__main__':
     x = int(input()) # i can be 0 to x
     y = int(input()) # j can be 0 to y 
     z = int(input()) # k can be 0 to z 
-    n = int(input())     
+    n = int(input()) #    
     # print a list of all possible coordinates 
     # condition : i + j + k != n 
     
@@ -10,4 +10,5 @@ if __name__ == '__main__':
     
     # newlist = [expression for item in iterable if condition == True]
     
-    
+    perm_list = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
+    print(perm_list)
