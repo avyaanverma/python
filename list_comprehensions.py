29701bc07a92e5536615bce16e3b2ac8f493308d
Avyaan Verma <114806843+avyaanverma@users.noreply.github.com>
2023-06-20 16:45:35 +0530

    ques list comprehension

---
+++ list_comprehensions.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    x = int(input()) # i can be 0 to x
+    y = int(input()) # j can be 0 to y 
+    z = int(input()) # k can be 0 to z 
+    n = int(input())     
+    # print a list of all possible coordinates 
+    # condition : i + j + k != n 
+    
+    # use list comprehensions rather than multiple loops
+    
+    # newlist = [expression for item in iterable if condition == True]
+    
+    
