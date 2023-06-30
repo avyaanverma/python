85814ca18146f45a033718e4901bb5f6ba54c943
Avyaan Verma <114806843+avyaanverma@users.noreply.github.com>
2023-06-30 12:25:54 +0530

    modified:   .gitignore

---
+++ objects_classes/Student.py
@@ -0,0 +1,8 @@
+class Student:
+
+    def __init__(self, name, major, gpa, is_on_probation): # Student data type
+        self.name = name
+        self.major = major
+        self.gpa   = gpa
+        self.is_on_probation = is_on_probation
+
---
+++ objects_classes/app.py
@@ -0,0 +1,7 @@
+from Student import Student
+
+student1 = Student("Jim", "Business", "3.1", False) # creating an object, an object is an instance of a class
+student2 = Student("Pam", "Art", "2.5", True) # creating an object, an object is an instance of a class
+
+print(student1.name)
+print(student2.name)
