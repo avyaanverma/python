c0f8a02ff9e360a258a6624c7cf22c8782945c30
Avyaan Verma <114806843+avyaanverma@users.noreply.github.com>
2023-07-03 22:16:37 +0530

    No guess game complete

--- game.py
+++ game.py
@@ -1,17 +1,28 @@
 import random
 import sys
-
-max_no=input("Level: ")
+while True:
+    try:
+        max_no=int(input("Level: "))
+        if max_no < 1: continue
+    except:
+        continue
+    else:
+        break
 
 random_no = random.randint(1,max_no)
-
-
 while True:
-    guess = input("Guess: ")
-    if guess < random_no:
-        print("Too small!")
-    elif guess > random_no:
-        print("Too large!")
-    else:
-        print("Just right")
-        sys.exit()
+    try:
+        guess = int(input("Guess: "))
+        if guess > max_no or guess < 1:
+            continue
+        else:
+            if guess < random_no:
+                print("Too small!")
+            elif guess > random_no:
+                print("Too large!")
+            else:
+                break
+    except:
+        continue
+
+sys.exit("Just right!")
