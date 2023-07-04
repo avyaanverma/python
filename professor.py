30c99b248cad1f4c35c01741e6d73e37727e34c2
Avyaan Verma <114806843+avyaanverma@users.noreply.github.com>
2023-07-04 16:13:01 +0530

    Program completed

--- professor.py
+++ professor.py
@@ -1,51 +1,43 @@
 import random
-
-
 def main():
+    correct = 0
     level = get_level()
-    # x + y = 10
-
     for _ in range(10):
         x = generate_integer(level)
-        y = generate_integer(levl)
-        value = input(f"{x} + {y} = ")
+        y = generate_integer(level)
+
         for __ in range(3):
+            value = int(input(f"{x} + {y} = "))
             if value == (x + y):
+                correct += 1
                 break
+            elif __ == 2:
+                print(f"{x + y}")
             else:
                 print("EEE")
-                print(f"{x+y}")
-
-
-
-
-
+        print(f"Score: {correct}")
 def get_level():
     lev = None
     while True:
         try:
-            lev = input("Level: ")
+            lev = int(input("Level: "))
             if lev not in [1,2,3]:
                 continue
         except ValueError:
-            raise ValueError
+            continue
         else:
             break
     return lev
-
-
-
 def generate_integer(level):
     if level == 1:
-        a = random.randint(1, 9)
+        a = random.randint(0, 9)
     elif level == 2:
-        a = random.randint(11, 99)
+        a = random.randint(10, 99)
     elif level == 3:
         a = random.randint(100, 999)
     else:
         raise ValueError
-
-
+    return a
 
 if __name__ == "__main__":
     main()
