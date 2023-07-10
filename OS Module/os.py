a22900cd3e2260884ad0abeef013a497076c6f38
Avyaan Verma <114806843+avyaanverma@users.noreply.github.com>
2023-07-10 20:18:31 +0530

    OS Library

---
+++ OS Module/os.py
@@ -0,0 +1,35 @@
+import os
+from datetime import datetime
+print(dir())
+
+# print(os.getcwd())
+# os.chdir('/Files/')
+# print(os.listdir())
+# print(os.getcwd())
+
+# creating a folder
+
+# os.makedirs('OS-test/test/test')
+# os.rmdir('OS-Demo-2')
+# os.removedirs('OS-test/test/test')
+
+
+# os.rename("original_file", "new_file")
+
+#print(os.stat('demo.txt').attribute
+
+#,mod_time=os.stat('demo.txt').st_mtime
+# print(datetime.fromtimestamp(mod_time))
+
+
+##os.walk() # generator that yields a tuple of three values as its walking the directory tree - dir path, dir within that path and files within that path
+
+# for dirpath,dirnames, filenames in os.walk("E:\Files\python"):
+#     print(dirpath)
+#     print(dirnames)
+#     print(filenames)
+#     print()
+#     print()
+
+
+
